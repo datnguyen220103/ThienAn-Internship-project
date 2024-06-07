d3b4e5d87303e88a2945562433efd417b75609f8
Đạt Nguyễn <168004919+datnguyen220103@users.noreply.github.com>
2024-06-08 02:26:35 +0700

    Add files via upload

---
+++ APIapp/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyWebApiApp.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Thêm chuỗi kết nối từ appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("MyDb");
+
+// Đăng ký DbContext với Dependency Injection
+builder.Services.AddDbContext<MyDbContext>(options =>
+    options.UseSqlServer(connectionString));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ APIapp/models/LoaiModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyWebApiApp.models
+{
+    public class LoaiModel
+    {
+        [Required]
+        [MaxLength(50)]
+        public string TenLoai { get; set; }
+    }
+}
