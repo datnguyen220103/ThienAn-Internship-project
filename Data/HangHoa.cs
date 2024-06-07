d398b1cef80ffe83c90af1e7ec11e8c92a0f57fa
Đạt Nguyễn <168004919+datnguyen220103@users.noreply.github.com>
2024-06-08 02:17:48 +0700

    Add files via upload

---
+++ Data/HangHoa.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyWebApiApp.Data
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public Guid MaHh { get; set; }
+        [Required]
+        [MaxLength(100)]
+        public string TenHh { get; set; }
+        public string Mota { get; set; }
+        [Range(0, double.MaxValue)]
+        public double Dongia { get; set; }
+
+        public byte GiamGia { get; set; }
+
+        public int? MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+    }
+}
